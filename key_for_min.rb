febcd2eb17009c0b7f665bc208b8f04ec71639c0
active-object-6703 <nerajno@gmail.com>
2019-03-25 02:13:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
- a =""
  b = 1000000
  name_hash.collect do | key, val|
   if val < b
     b= val
     a = key
-  end
+  endleamr
  end
  a
 end
